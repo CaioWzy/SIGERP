a864ce405805ebd7e3e28ad414350944c0f6116e
CaioWzy <CaioWzy@gmail.com>
2020-02-24 22:00:32 -0300

    Implementa componente SearchBox

---
+++ components/SearchBox.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="input-group input-group--custom">
+    <input
+      type="text"
+      class="form-control form-control--custom"
+      :class="{'searchbox--active': hasChars }"
+      v-model="SearchInput"
+      @change="checkHasChars"
+      placeholder="Pesquisar"
+    />
+  </div>
+</template>
+
+<style>
+.form-control--custom {
+  background: rgba(0, 0, 0, 0.15);
+  border: none;
+}
+
+.form-control--custom:focus {
+  box-shadow: none;
+  border: none;
+}
+
+.form-control--custom::placeholder {
+  color: #fff;
+}
+
+.searchbox--active {
+  background: #fff !important;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      hasChars: false,
+      SearchInput: ""
+    };
+  },
+  methods: {
+    checkHasChars() {
+      if (this.SearchInput.trim() == "") {
+        this.hasChars = false;
+      } else {
+        this.hasChars = true;
+      }
+    }
+  }
+};
+</script>
