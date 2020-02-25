2141d45755ec244bd92b3a22ae3226695fe21f6a
CaioWzy <CaioWzy@gmail.com>
2020-02-25 17:11:47 -0300

    Implementa componente base para listagem de páginas

---
+++ components/ListPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="page-header">
+      <div class="container py-1">
+        <div class="row">
+          <div class="col">
+            <slot name="title"></slot>
+          </div>
+          <div class="col">
+            <div class="controls float-right">
+              <slot name="controls"></slot>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container mt-4">
+      <div class="row">
+        <slot name="table"></slot>
+      </div>
+    </div>
+  </div>
+</template>
