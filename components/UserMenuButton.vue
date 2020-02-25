0d4dd0358ad8d35117fb482bb904d8997f19979c
CaioWzy <CaioWzy@gmail.com>
2020-02-25 15:10:40 -0300

    Implementa UserButton

---
+++ components/UserMenuButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-dropdown
+    type="button"
+    right
+    class="btn btn-control custom-dropdown float-right"
+    toggle-class="btn-custom btn-user"
+  >
+    <template v-slot:button-content>
+      <font-awesome-icon :icon="['fas', 'user']" />
+    </template>
+    <b-dropdown-item>
+      <div class="row">
+        <div class="col">Sair</div>
+        <div class="col">
+          <div class="float-right">
+            <font-awesome-icon :icon="['fas', 'sign-out-alt']" />
+          </div>
+        </div>
+      </div>
+    </b-dropdown-item>
+  </b-dropdown>
+</template>
+
+<style>
+.custom-dropdown {
+  padding: 0;
+}
+
+.btn-user {
+  padding: 0;
+}
+.btn-user:hover {
+  color: #2980b9 !important;
+}
+.btn-user::after {
+  content: none;
+}
+
+.show > .btn-user.dropdown-toggle {
+  color: #2980b9 !important;
+  background: #fff !important;
+}
+</style>
