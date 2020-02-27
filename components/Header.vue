56e3de23e620ca95d851947e0b22afb637388382
CaioWzy <CaioWzy@gmail.com>
2020-02-27 14:20:03 -0300

    Refatora navbar

---
+++ components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="page-header">
+    <div class="container pb-1">
+      <div class="row">
+        <div class="col">
+          <slot name="title"></slot>
+        </div>
+        <div class="col">
+          <div class="controls float-right">
+            <slot name="controls"></slot>
+            <b-button @click="$bvModal.show('modalFormDefault')" class="btn btn-custom btn-add">
+              <template v-if="!useEditIcon">
+                <font-awesome-icon :icon="['fas', 'plus']" />
+              </template>
+              <template v-else>
+                <font-awesome-icon :icon="['fas', 'edit']" />
+              </template>
+            </b-button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    useEditIcon: {
+      type: Boolean
+    }
+  }
+};
+</script>
---
+++ components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="page-header">
+    <div class="container py-2">
+      <div class="row">
+        <div class="col-1">
+          <template v-if="!disableGoBack">
+            <button @click="goBack" type="button" class="btn btn-control btn-custom btn-back">
+              <font-awesome-icon :icon="['fas', 'angle-left']" />
+            </button>
+          </template>
+        </div>
+        <div class="col">
+          <SearchBox v-if="enableSearchBox" />
+        </div>
+        <div class="col-1">
+          <UserMenuButton />
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import UserMenuButton from "~/components/UserMenuButton";
+import SearchBox from "~/components/SearchBox";
+
+export default {
+  components: {
+    UserMenuButton,
+    SearchBox
+  },
+  props: {
+    enableGoBack: Boolean,
+    enableSearchBox: Boolean
+  },
+  methods: {
+    goBack() {
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.push("/");
+    }
+  }
+};
+</script>
