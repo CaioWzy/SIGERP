e3d52e85e94dde705ff616e6512b07194de0ace1
CaioWzy <CaioWzy@gmail.com>
2020-03-01 18:20:11 -0300

    Usa componente VSelect no Formulário

---
+++ components/Funcionario/Form/index.vue
@@ -0,0 +1,136 @@
+<template>
+  <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+    <div class="form-row">
+      <div class="col">
+        <b-form-group id="input-group-name" label="Nome:" label-for="input-name">
+          <b-form-input
+            id="input-name"
+            v-model="funcionario.name"
+            required
+            placeholder="Fulano da Silva"
+            maxlength="120"
+          ></b-form-input>
+        </b-form-group>
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="col">
+        <b-form-group id="input-group-cpf" label="CPF:" label-for="input-cpf">
+          <b-form-input
+            id="input-cpf"
+            v-model="funcionario.cpf"
+            required
+            placeholder="123.456.789-10"
+            maxlength="13"
+            v-mask="'###.###.###-##'"
+          ></b-form-input>
+        </b-form-group>
+      </div>
+      <div class="col">
+        <b-form-group
+          id="input-group-date_of_birth"
+          label="Data de Nascimento:"
+          label-for="input-date_of_birth"
+        >
+          <b-form-datepicker id="input-date_of_birth" v-model="funcionario.date_of_birth" required></b-form-datepicker>
+        </b-form-group>
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="col">
+        <b-form-group id="input-group-email" label="E-mail:" label-for="input-email">
+          <b-form-input
+            id="input-email"
+            v-model="funcionario.email"
+            required
+            placeholder="fulano@site.com.br"
+            maxlength="120"
+          ></b-form-input>
+        </b-form-group>
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="col">
+        <ClientesVSelect ref="empresas" />
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="col">
+        <b-form-group id="input-group-enrolment" label="Matrícula:" label-for="input-enrolment">
+          <b-form-input
+            id="input-enrolment"
+            v-model="funcionario.enrolment"
+            required
+            placeholder="202002270001"
+            maxlength="120"
+          ></b-form-input>
+        </b-form-group>
+      </div>
+      <div class="col">
+        <b-form-group
+          id="input-group-admission_date"
+          label="Data de Admissão:"
+          label-for="input-admission_date"
+        >
+          <b-form-datepicker
+            id="input-admission_date"
+            v-model="funcionario.admission_date"
+            today-button
+            required
+          ></b-form-datepicker>
+        </b-form-group>
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="col">
+        <b-button type="submit" class="btn float-right" variant="primary">
+          <font-awesome-icon :icon="['fas', 'save']" />
+        </b-button>
+      </div>
+    </div>
+  </b-form>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+import { mapActions } from "vuex";
+
+import { mask } from "vue-the-mask";
+
+import ClientesVSelect from "./ClientesVSelect";
+
+export default {
+  directives: { mask },
+  components: {
+    ClientesVSelect
+  },
+  data() {
+    return {
+      funcionario: { ...this.$store.state.pages.data },
+      show: true
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      if (!this.funcionario.id) this.create(this.funcionario);
+      else this.update(this.funcionario);
+    },
+    onReset(e) {
+      e.preventDefault();
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+    ...mapActions({
+      create: "pages/create",
+      update: "pages/update"
+    }),
+    ...mapMutations({
+      reset: "pages/reset"
+    })
+  }
+};
+</script>
